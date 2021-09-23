c99e4d5b6658bce67818d92b3b34cf3c317d459a
Amorem <dev@amorem.com>
2021-09-23 15:20:42 +0200

    fix bad gitIgnore

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import Nav from "./Nav";
+
+function Layout({ children }) {
+  return (
+    <div className="flex flex-col justify-between min-h-screen">
+      <Nav />
+      <main>{children}</main>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default Layout;
