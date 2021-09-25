5082f52d5fa2710fbd2e88f83883a8f7b9435246
Amorem <dev@amorem.com>
2021-09-25 12:56:44 +0200

    SEO

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,39 @@
 import { getProductsInCollection } from "../lib/shopify";
 import ProductList from "../components/ProductList";
 import Hero from "../components/Hero";
+import Head from "next/head";
 
 export default function Home({ products }) {
   return (
     <div className="">
+      <Head>
+        <title>Modern eCommerce Example</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+          http-equiv="Content-Type"
+          content="text/html; charset=ISO-8859-1"
+        />
+        <meta
+          name="description"
+          content="Modern eCommerce Example focusing on Shopify, NextJS, TailwindCSS, and GraphQL "
+        />
+        <meta property="og:title" content="Modern eCommerce Example" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://vercel.com/amorem/shopify-nextjs-tailwind"
+        />
+        <meta
+          property="og:image"
+          content="https://vercel.com/amorem/shopify-nextjs-tailwind/images/share.png"
+        />
+        <meta
+          property="og:description"
+          content="Modern eCommerce Example focusing on Shopify, NextJS, TailwindCSS, and GraphQL "
+        />
+        <meta property="og:locale" content="en-US" />
+        <meta property="og:site_name" content="Modern eCommerce Example" />
+      </Head>
       <Hero />
       <ProductList products={products} />
     </div>
